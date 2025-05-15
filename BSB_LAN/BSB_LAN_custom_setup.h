899a5dc845a58adeddac137d1e3c9a36180bfe59
Tom Carpenter <T.Carpenter@leeds.ac.uk>
2025-05-15 21:04:04 +0100

    Add Custom Setup and amend Default Config
    This is intended to support use with the PCB designs in the TCWORLD/BSB-Interface-PCBs repository. The auto-discover of pins from 0,0 doesn't work for these boards because they instead use an extra GPIO pin to control whether the TX line is enabled.

--- BSB_LAN/BSB_LAN_custom_setup.h
+++ BSB_LAN/BSB_LAN_custom_setup.h
@@ -1,2 +1,7 @@
 // Add custom code for setup function here which will be included at the end of the function
 
+// Set TX Enable pin to output and set high now that the system is initialised and the
+// TX line is being driven correctly
+pinMode(32, OUTPUT);
+digitalWrite(32, HIGH);
+
