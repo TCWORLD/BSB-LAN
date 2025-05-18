3fa8629aa344483a6cd752ca4fc201ec703e0e64
Tom Carpenter <T.Carpenter@leeds.ac.uk>
2025-05-18 22:37:38 +0100

    Add Defines for Everblu Frequency Parameters
    Make it easier to configure to requirements for sweep and min/max allowed settings.

--- BSB_LAN/src/everblu-meter/everblu_config.h
+++ BSB_LAN/src/everblu-meter/everblu_config.h
@@ -5,8 +5,16 @@
 // Meter-specific configuration
 #define METER_YEAR 13        // Last two digits of the year printed on the meter (e.g., 2019 is 19)
 #define METER_SERIAL 535395  // Meter Serial Number (omit leading zero)
-#define FREQUENCY 433.700007 // Frequency of the meter (default. Can be discovered via test code)
 #define GDO0 35              // Pin used for GDO0 (General Digital Output 0)
+#define FREQUENCY 433.76f    // Frequency of the meter (default. Can be discovered via test code)
+
+#define CC1101_FREQUENCY_MIN 433.0f // Minimum allowed frequency for CC1101 variant
+#define CC1101_FREQUENCY_MAX 434.0f // Maximum allowed frequency for CC1101 variant
+
+#define SWEEP_FREQUENCY_MIN 433.70f // Sweep frequency range lower limit
+#define SWEEP_FREQUENCY_STEP 0.001f // Sweep frequency step
+#define SWEEP_FREQUENCY_MAX 433.98f // Sweep frequency range upper limit
+
 
 #define METER_READ_HOUR  9   // Hour to start trying to read meter
 
