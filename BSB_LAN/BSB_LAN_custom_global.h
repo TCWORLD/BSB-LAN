7d58f47ee2e25cfd2146d2ffc79cbdfd4887b47c
Tom Carpenter <T.Carpenter@leeds.ac.uk>
2025-05-23 11:17:19 +0100

    Add Everblu Cycle Water Meter Custom Logic to BSB-LAN

--- BSB_LAN/BSB_LAN_custom_global.h
+++ BSB_LAN/BSB_LAN_custom_global.h
@@ -1,2 +1,5 @@
 // Add custom code for global functions here which will be included in the global section
 
+#include "src/everblu-meter/everblu_meters.h" // Include EverBlu meter communication library
+
+bool has_everblu = false;
--- BSB_LAN/BSB_LAN_custom_setup.h
+++ BSB_LAN/BSB_LAN_custom_setup.h
@@ -5,3 +5,11 @@
 pinMode(32, OUTPUT);
 digitalWrite(32, HIGH);
 
+// Initialise water meter reader
+static_assert(sizeof(everblu_config_t) <= sizeof(custom_eeprom));
+everblu_setConfig((everblu_config_t*)&custom_eeprom);
+has_everblu = everblu_initialise(custom_longs);
+// Save the frequency to MQTT line
+if (has_everblu) custom_floats[0] = everblu_getFrequency();
+else custom_floats[0] = -1.0f;
+
